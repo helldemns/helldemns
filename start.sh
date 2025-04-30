1eb2968a6fb64b7fe797490753d15fc3cda8cf48
macOS <macos@macOSs-MacBook-Pro.local>
2025-04-30 18:31:08 +0700

    Add Procfile and start script for Railway

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-composer install
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
 cp .env.example .env
+
 php artisan key:generate
 php artisan migrate --force
+
 php artisan serve --host=0.0.0.0 --port=8080
